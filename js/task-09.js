d06752bd1c7f7684b08d922b502c5fc651f07227
daria-kuznecova <kovalenko18101990@gmail.com>
2023-04-23 14:13:28 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorEl = document.querySelector(".change-color");
+const textColorEl = document.querySelector(".color");
+
+changeColorEl.addEventListener("click", () => {
+  let hexColor = getRandomHexColor();
+  document.body.style.backgroundColor = hexColor;
+  textColorEl.textContent = hexColor;
+});
