058231ad44b0b311266d904146722b58b7c1b854
daria-kuznecova <kovalenko18101990@gmail.com>
2023-04-24 16:43:18 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,25 +6,11 @@ form.addEventListener("submit", (event) => {
     elements: { email, password },
   } = event.currentTarget;
 
-  // const formEl = {
-  //   elementsEmail: email.value,
-  //   elementsPassword: password.value,
-  // };
-
-  // console.log(formEl);
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
 
   console.log({ email: email.value, password: password.value });
 
-  if (email.value === "" || password.value === "") {
-    alert("Please fill in all the fields!");
-  } else return;
-
   event.currentTarget.reset();
 });
-
-// const inputs = document.querySelectorAll("input");
-// inputs.forEach((input) => {
-//   if (input.value.trim() === "") {
-//     alert("Error");
-//   }
-// });
